1912d0b64b4d6c6b2594e7df166282d3d8466251
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-04-14 10:31:57 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,27 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    d = b ** 2 - 4 * a * c
+
+    if d > 0:
+        r1 = (-b + d ** 0.5) / (2 * a)
+        r2 = (-b - d ** 0.5) / (2 * a)
+        return f"({r1}, {r2})"
+    elif d == 0:
+        r = -b / (2 * a)
+        return f"({r})"
+    else:
+        return "( )"
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+
+    return a * x ** 2 + b * x + c
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    return f"f(x) = {a} * x^2 + {b} * x + {c}"
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    return f"f'(x) = {2 * a}x + {b}"
